c2b48e54ead0d698c29140b33f21f51275747cc2
Francisca Benavides <fbenavidesca@outlook.com>
2023-04-17 16:42:09 -0400

    Se Agrega a opcion 2 a la app y se verifican errores en lo que figura en pantalla, por tanto, se actualiza verAlojamientos() de EmpresaTurismo.java para obtener los resultados esperados

--- src/controlador/EmpresaTurismo.java
+++ src/controlador/EmpresaTurismo.java
@@ -24,85 +24,133 @@ public class EmpresaTurismo {
                 return i;}}
         return -1;} //No se encontro cte
 
-    //Ingresar Cabaña
-    public void ingresarAlojamientoCabagna(Cabagna cabagna){
-        if(buscarCte(cabagna.getDatosCliente().getRut())==-1){
-            empresaTurismo.add(cabagna);}}
+        //Ingresar Cabaña
+        public void ingresarAlojamientoCabagna(Cabagna cabagna){
+            if(buscarCte(cabagna.getDatosCliente().getRut())==-1){
+                empresaTurismo.add(cabagna);}}
 
-    //Ingresar Hotel
-    public void ingresarAlojamientoHotel(Hotel hotel){
-        if(buscarCte(hotel.getDatosCliente().getRut())==-1){
-            empresaTurismo.add(hotel);}}
+        //Ingresar Hotel
+        public void ingresarAlojamientoHotel(Hotel hotel){
+            if(buscarCte(hotel.getDatosCliente().getRut())==-1){
+                empresaTurismo.add(hotel);}}
 
-    //Ingresar Carpa
-    public void ingresarAlojamientoCarpa(Carpa carpa){
-        if(buscarCte(carpa.getDatosCliente().getRut())==-1){
-            empresaTurismo.add(carpa);}}
+        //Ingresar Carpa
+        public void ingresarAlojamientoCarpa(Carpa carpa){
+            if(buscarCte(carpa.getDatosCliente().getRut())==-1){
+                empresaTurismo.add(carpa);}}
 
 
     public String verAlojamientos() {
-        String msj = ""; Carpa carpa = null; Cabagna cabagna = null; Hotel hotel = null;
+        String msj = "";
         int contador = 0;
-        for (int i = 0; i < empresaTurismo.size(); i++) {
-            if (empresaTurismo.get(i) instanceof Hotel) {
-                hotel = (Hotel) empresaTurismo.get(i);
-                msj = "\n Nombre cliente: "+ hotel.getDatosCliente().getNombre() + "\n El rut del cliente es: "+ hotel.getDatosCliente().getRut() + "\n Temporada: " + hotel.getTipoTemporada() +  "\n Cantidad de noches: " + hotel.getCantidadNoches() + "\n Valor base noche: " + hotel.getValorBaseNoche() + "\n Capacidad: " + hotel.getCapacidad() + "\n Es fumador: " + hotel.isEsFumador() + "\n Con desayuno: " + hotel.isConDesayuno();
-                 contador++;
-            } else if (empresaTurismo.get(i) instanceof Cabagna) {
-                cabagna = (Cabagna) empresaTurismo.get(i);
-                msj = "\n Nombre cliente: "+ cabagna.getDatosCliente().getNombre() + "\n El rut del cliente es: "+ cabagna.getDatosCliente().getRut() + "\n Temporada: " + cabagna.getTipoTemporada() + "\n Cantidad de noches: " + cabagna.getCantidadNoches() + "\n Valor base noche: " + cabagna.getValorBaseNoche() + "\n Capacidad: " + cabagna.getCapacidad() + "\n Con chimenea: " + cabagna.isChimenea();
+
+        for (MedioDeAlojamiento alojamiento : empresaTurismo) {
+            String tipoAlojamiento = "";
+            if (alojamiento instanceof Hotel) {
+                tipoAlojamiento = "Hotel";
+                Hotel hotel = (Hotel) alojamiento;
+                msj += "\nTipo de alojamiento: Hotel" +
+                        "\n Nombre cliente: " + hotel.getDatosCliente().getNombre() +
+                        "\n El rut del cliente es: " + hotel.getDatosCliente().getRut() +
+                        "\n Temporada: " + hotel.getTipoTemporada() +
+                        "\n Cantidad de noches: " + hotel.getCantidadNoches() +
+                        "\n Valor base noche: " + hotel.getValorBaseNoche() +
+                        "\n Capacidad: " + hotel.getCapacidad() +
+                        "\n Es fumador: " + hotel.isEsFumador() +
+                        "\n Con desayuno: " + hotel.isConDesayuno() +
+                        "\n\n";
                 contador++;
-            } else if (empresaTurismo.get(i) instanceof Carpa) {
-                carpa = (Carpa) empresaTurismo.get(i);
-                msj = "\n Nombre cliente: "+ carpa.getDatosCliente().getNombre() + "\n El rut del cliente es: "+ carpa.getDatosCliente().getRut() + "\n Temporada: " + carpa.getTipoTemporada() + "\n Cantidad de noches: " + carpa.getCantidadNoches() + "\n Valor base noche: " + carpa.getValorBaseNoche() + "\n Cantidad de personas: " + carpa.getCantidadPersonas();
+            } else if (alojamiento instanceof Cabagna) {
+                tipoAlojamiento = "Cabagna";
+                Cabagna cabagna = (Cabagna) alojamiento;
+                msj += "\nTipo de alojamiento: Cabania" +
+                        "\n Nombre cliente: " + cabagna.getDatosCliente().getNombre() +
+                        "\n El rut del cliente es: " + cabagna.getDatosCliente().getRut() +
+                        "\n Temporada: " + cabagna.getTipoTemporada() +
+                        "\n Cantidad de noches: " + cabagna.getCantidadNoches() +
+                        "\n Valor base noche: " + cabagna.getValorBaseNoche() +
+                        "\n Capacidad: " + cabagna.getCapacidad() +
+                        "\n Con chimenea: " + cabagna.isChimenea() +
+                        "\n\n";
+                contador++;
+            } else if (alojamiento instanceof Carpa) {
+                tipoAlojamiento = "Carpa";
+                Carpa carpa = (Carpa) alojamiento;
+                msj += "\nTipo de alojamiento: Carpa" +
+                        "\n Nombre cliente: " + carpa.getDatosCliente().getNombre() +
+                        "\n El rut del cliente es: " + carpa.getDatosCliente().getRut() +
+                        "\n Temporada: " + carpa.getTipoTemporada() +
+                        "\n Cantidad de noches: " + carpa.getCantidadNoches() +
+                        "\n Valor base noche: " + carpa.getValorBaseNoche() +
+                        "\n Cantidad de personas: " + carpa.getCantidadPersonas() +
+                        "\n\n";
                 contador++;
             }
         }
-        if (contador == 0)
+        if (contador == 0) {
             System.out.println("No se encontraron alojamientos para el rut ingresado");
+        }
         return msj;
-
     }
+
+
 
 
     public void mostrarCliente(String rut) {
         MedioDeAlojamiento alojamientoEncontrado = null;
         for (int i = 0; i < empresaTurismo.size(); i++) {
-            MedioDeAlojamiento alojamiento = empresaTurismo.get(i); {
-            if (alojamiento.getDatosCliente().getRut().compareToIgnoreCase(rut) == 0) {
-                alojamientoEncontrado = alojamiento;
-                break;
+            MedioDeAlojamiento alojamiento = empresaTurismo.get(i);
+            {
+                if (alojamiento.getDatosCliente().getRut().compareToIgnoreCase(rut) == 0) {
+                    alojamientoEncontrado = alojamiento;
+                    break;
+                }
             }
-        }
-        if (alojamientoEncontrado != null) {
-            System.out.println("Datos del cliente:");
-            System.out.println("Nombre: " + alojamientoEncontrado.getDatosCliente().getNombre());
-            System.out.println("Rut: " + alojamientoEncontrado.getDatosCliente().getRut());
-            System.out.println("Alojado en: " + alojamientoEncontrado.getClass().getSimpleName()); // se usa el metodo getClass() para obtener la clase del objeto y el metodo getSimpleName() para obtener el nombre simple de la clase.
-            System.out.println("Temporada: " + alojamientoEncontrado.getTipoTemporada());
-            System.out.println("Descuento de acuerdo a temporada: " + alojamientoEncontrado.bonoDescuento());
-            System.out.println("Cantidad de noches: " + alojamientoEncontrado.getCantidadNoches());
-            System.out.println("Valor base por noche: " + alojamientoEncontrado.getValorBaseNoche());;
-            System.out.println("Total a pagar: $" + alojamientoEncontrado.valorACancelar());
-            if (alojamientoEncontrado instanceof Carpa) {
-                System.out.println("Cantidad de personas: " + ((Carpa) alojamientoEncontrado).getCantidadPersonas());
-            } else if (alojamientoEncontrado instanceof Hotel) {
-                System.out.println("Capacidad: " + ((Hotel) alojamientoEncontrado).getCapacidad());
-                System.out.println("Con desayuno: " + ((Hotel) alojamientoEncontrado).isConDesayuno());
-                System.out.println("Es fumador: " + ((Hotel) alojamientoEncontrado).isEsFumador());
-            } else if (alojamientoEncontrado instanceof Cabagna) {
-                System.out.println("Capacidad: " + ((Cabagna) alojamientoEncontrado).getCapacidad());
-                System.out.println("Con chimenea: " + ((Cabagna) alojamientoEncontrado).isChimenea());
-                System.out.println("Es fumador: " + ((Cabagna) alojamientoEncontrado).isEsFumador());
+            if (alojamientoEncontrado != null) {
+                System.out.println("Datos del cliente:");
+                System.out.println("Nombre: " + alojamientoEncontrado.getDatosCliente().getNombre());
+                System.out.println("Rut: " + alojamientoEncontrado.getDatosCliente().getRut());
+                System.out.println("Alojado en: " + alojamientoEncontrado.getClass().getSimpleName()); // se usa el metodo getClass() para obtener la clase del objeto y el metodo getSimpleName() para obtener el nombre simple de la clase.
+                System.out.println("Temporada: " + alojamientoEncontrado.getTipoTemporada());
+                System.out.println("Descuento de acuerdo a temporada: " + alojamientoEncontrado.bonoDescuento());
+                System.out.println("Cantidad de noches: " + alojamientoEncontrado.getCantidadNoches());
+                System.out.println("Valor base por noche: " + alojamientoEncontrado.getValorBaseNoche());
+                ;
+                System.out.println("Total a pagar: $" + alojamientoEncontrado.valorACancelar());
+                if (alojamientoEncontrado instanceof Carpa) {
+                    System.out.println("Cantidad de personas: " + ((Carpa) alojamientoEncontrado).getCantidadPersonas());
+                } else if (alojamientoEncontrado instanceof Hotel) {
+                    System.out.println("Capacidad: " + ((Hotel) alojamientoEncontrado).getCapacidad());
+                    System.out.println("Con desayuno: " + ((Hotel) alojamientoEncontrado).isConDesayuno());
+                    System.out.println("Es fumador: " + ((Hotel) alojamientoEncontrado).isEsFumador());
+                } else if (alojamientoEncontrado instanceof Cabagna) {
+                    System.out.println("Capacidad: " + ((Cabagna) alojamientoEncontrado).getCapacidad());
+                    System.out.println("Con chimenea: " + ((Cabagna) alojamientoEncontrado).isChimenea());
+                    System.out.println("Es fumador: " + ((Cabagna) alojamientoEncontrado).isEsFumador());
+                }
+            } else {
+                System.out.println("Cliente no encontrado.");
             }
-        } else {
-            System.out.println("Cliente no encontrado.");
-        }
+        }}
+
+
+    public int totalAdicional() {
+        return empresaTurismo.stream() //devuelve un flujo de elementos de la colección
+                .filter(alojamiento -> alojamiento instanceof Hotel)//filtra solo los elementos de clase Hotel
+                .mapToInt(alojamiento -> ((Hotel) alojamiento).adicional())//Transforma cada elemento en su valor adicional correspondiente
+                .sum();//suma todos los valores adicionales
     }
 
 
-    //Seguir aqui
+    public int totalBonoDescuento() {
+        int bonoDescuento = empresaTurismo.stream()
+                .filter(alojamiento -> alojamiento instanceof Carpa || alojamiento instanceof Hotel || alojamiento instanceof Cabagna) //filtra los elementos de las 3 clases
+                .mapToInt(MedioDeAlojamiento::bonoDescuento)
+                .sum();
+        return bonoDescuento;
+    }
 
 
-}
+
+
 }
--- src/vista/App.java
+++ src/vista/App.java
@@ -1,23 +1,77 @@
 package vista;
+import controlador.EmpresaTurismo;
 import modelo.*;
 
+import java.util.ArrayList;
+
+
 public class App {
-
-    public static void main(String []args){
-
-        Carpa carpa = new Carpa (new DatosCliente("Francisca Benavides", "18326662-4"), 8,20000, "media", 8);
+    public static void main(String[] args) {
+        EmpresaTurismo empresaTurismo = new EmpresaTurismo(new ArrayList<>());
 
         Hotel hotel = new Hotel(new DatosCliente("Nobara Cat", "20222554-8"), 50000, 15, "baja", true, 2, false);
+        Cabagna cabagna = new Cabagna(new DatosCliente("Francisca", "18326662-4"), 8000, 5, "baja", false, 2 );
+        Carpa carpa = new Carpa(new DatosCliente("Vivi", "11133400-5"), 16000, 4, "alta", 4);
 
-        Cabagna cabagna = new Cabagna(new DatosCliente("Ricardo Nuñez", "17625542-K"), 40000, 5,"media", false, 8, true);;
+        empresaTurismo.ingresarAlojamientoHotel(hotel);
+        //empresaTurismo.ingresarAlojamientoCabagna(cabagna);
+        //empresaTurismo.ingresarAlojamientoCarpa(carpa);
 
-        System.out.println("El subtotal de la cabaña es: $"+cabagna.subTotal());
-        System.out.println("El bono de descuento de la cabaña es: $"+cabagna.bonoDescuento());
-        System.out.println("El total a pagar de la cabaña es: $"+cabagna.valorACancelar());
-        System.out.println("El incremento en el valor base es de: $"+cabagna.incrementaValorBase());
+        int opcion;
+        do {
+                opcion = menu();
+                switch(opcion){
+                    case 1:
+                        System.out.println("Ingreso medio de alojamiento");
+                        break;
+                    case 2:
+                        if(empresaTurismo.getEmpresaTurismo().size()==0){
+                            System.out.println("No hay alojamientos ingresados en nuestro sistema");
+                        }else {
+                            System.out.println(empresaTurismo.verAlojamientos());
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Mostrar datos de cliente");
+                        break;
+                    case 4:
+                        System.out.println("Total adicional");
+                        break;
+                    case 5:
+                        System.out.println("Total bono descuento");
+                        break;
+                    case 6:
+                        System.out.println("Cantidad medios de alojamiento");
+                        break;
+                    case 7:
+                        System.out.println("Valor a cancelar");
+                        break;
+                    case 8:
+                        System.out.println("Aplicar incremento del valor base");
+                        break;
+
+                }
+            } while (opcion != 9);
 
     }
+    public static int menu(){
+        System.out.println("Empresa de Turismo 'Valle grande' v1 ");
+        System.out.println("1.\tIngresar Medio de Alojamiento");
+        System.out.println("2.\tMostrar medios de alojamiento");
+        System.out.println("3.\tDatos de un cliente");
+        System.out.println("4.\tTotal adicional");
+        System.out.println("5.\tTotal bono descuento");
+        System.out.println("6.\tCantidad medios de alojamiento");
+        System.out.println("7.\tValor a cancelar por un cliente");
+        System.out.println("8.\tAplicar incremento del valor base");
+        System.out.println("9.\tSalir");
+        System.out.println("Por favor ingrese una opcion para continuar...");
 
+        return Leer.datoInt();
 
-
+    }
+    public static String pedirRut(){
+        System.out.println("Favor ingrese rut del cliente (Ej: 12345678-9)");
+        return Leer.dato();
+    }
 }
